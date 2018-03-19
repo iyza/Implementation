917fdbe72d1533cf8ac7c73e78e7a0399e8d31ac
Luka Bajkin <lulyhead@gmail.com>
2018-03-19 14:07:32 -0400

    commit

---
+++ src/Lists/ArrayList.java
@@ -0,0 +1,17 @@
+package Lists;
+
+/**
+ * Created by bajlu1731402 on 2018-03-19.
+ */
+public class ArrayList<T> {
+
+    private T[] tableau;
+    private int taille = 0;
+    private int objets = 0;
+
+    public void add(T element) {
+        if (taille > objets) {
+
+        }
+    }
+}
