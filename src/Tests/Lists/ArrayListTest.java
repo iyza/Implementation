b170891b69129f7f4e438859b5735552de594b3d
Luka Bajkin <lulyhead@gmail.com>
2018-03-29 13:39:43 -0400

    commit

---
+++ src/Tests/Lists/ArrayListTest.java
@@ -0,0 +1,41 @@
+package Lists;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by bajlu1731402 on 2018-03-29.
+ */
+class ArrayListTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void set() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+}
